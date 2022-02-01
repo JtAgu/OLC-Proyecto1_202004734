e0b52ddf3ddbdd44ac37ad8213236112cc9b626b
Justin Aguirre <justinaguirre59@gmail.com>
2022-02-01 09:19:19 -0600

    inicio

---
+++ [OLC]Proyecto1_202004734/src/olc/proyecto1_202004734/Principal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olc.proyecto1_202004734;
+
+/**
+ *
+ * @author justin
+ */
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
